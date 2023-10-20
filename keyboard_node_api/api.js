5df969cbd6570bdffdbef696547f19e570a34fc5
blackarm0815 <honungsbjorn@protonmail.com>
2023-10-20 13:26:16 +0100

    trying to add in flashing led code

--- keyboard_node_api/api.js
+++ keyboard_node_api/api.js
@@ -1,11 +1,19 @@
 // this is a security thing is needed for the webserver and api to work on the same machine
 const cors = require('cors');
+// node-rpio
+const rpio = require('../lib/rpio');
 
 // express.js handles all the api stuff
 const express = require('express');
 const app = express();
 
-let morseString = '';
+/*
+* Configure the pin for output, setting it low initially.  The state is set
+* prior to the pin being activated, so is suitable for devices which require
+* a stable setup.
+*/
+const pin = 37;
+rpio.open(pin, rpio.OUTPUT, rpio.LOW);
 
 // allow the website
 app.use(cors({
@@ -20,228 +28,23 @@ app.get('/', (req, res) => {
   res.send('<html>hello world<html>');
 });
 
-app.get('/keya', (req, res) => {
+app.get('/blink', (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'a';
-  console.log(morseString);
-  res.send('<html>a<html>');
-});
-
-app.get('/keyb', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'b';
-  console.log(morseString);
-  res.send('<html>b<html>');
-});
-
-app.get('/keyc', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'c';
-  console.log(morseString);
-  res.send('<html>c<html>');
-});
-
-app.get('/keyd', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'd';
-  console.log(morseString);
-  res.send('<html>d<html>');
-});
-
-app.get('/keye', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'e';
-  console.log(morseString);
-  res.send('<html>e<html>');
-});
-
-app.get('/keyf', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'f';
-  console.log(morseString);
-  res.send('<html>f<html>');
-});
-
-app.get('/keyg', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'g';
-  console.log(morseString);
-  res.send('<html>g<html>');
-});
-
-app.get('/keyh', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'h';
-  console.log(morseString);
-  res.send('<html>h<html>');
-});
-
-app.get('/keyi', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'i';
-  console.log(morseString);
-  res.send('<html>i<html>');
-});
-
-app.get('/keyj', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'j';
-  console.log(morseString);
-  res.send('<html>j<html>');
-});
-
-app.get('/keyk', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'k';
-  console.log(morseString);
-  res.send('<html>k<html>');
-});
-
-app.get('/keyl', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'l';
-  console.log(morseString);
-  res.send('<html>l<html>');
-});
-
-app.get('/keym', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'm';
-  console.log(morseString);
-  res.send('<html>m<html>');
-});
-
-app.get('/keyn', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'n';
-  console.log(morseString);
-  res.send('<html>n<html>');
-});
-
-app.get('/keyo', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'o';
-  console.log(morseString);
-  res.send('<html>o<html>');
-});
-
-app.get('/keyp', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'p';
-  console.log(morseString);
-  res.send('<html>p<html>');
-});
-
-app.get('/keyq', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'q';
-  console.log(morseString);
-  res.send('<html>q<html>');
-});
-
-app.get('/keyr', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'r';
-  console.log(morseString);
-  res.send('<html>r<html>');
-});
-
-app.get('/keys', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 's';
-  console.log(morseString);
-  res.send('<html>s<html>');
-});
-
-app.get('/keyt', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 't';
-  console.log(morseString);
-  res.send('<html>t<html>');
-});
-
-app.get('/keyu', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'u';
-  console.log(morseString);
-  res.send('<html>u<html>');
-});
-
-app.get('/keyv', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'v';
-  console.log(morseString);
-  res.send('<html>v<html>');
-});
-
-app.get('/keyw', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'w';
-  console.log(morseString);
-  res.send('<html>w<html>');
-});
-
-app.get('/keyx', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'x';
-  console.log(morseString);
-  res.send('<html>x<html>');
-});
-
-app.get('/keyy', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'y';
-  console.log(morseString);
-  res.send('<html>y<html>');
-});
-
-app.get('/keyz', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += 'z';
-  console.log(morseString);
-  res.send('<html>z<html>');
-});
-
-app.get('/keyspace', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString += ' ';
-  console.log(morseString);
-  res.send('<html>space<html>');
-});
-
-app.get('/keyenter', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  morseString = '';
-  console.log('**** message over ****');
-  res.send('<html>enter<html>');
+  /*
+   * Blink the LED 5 times.  The sleep functions block, but for a trivial example
+   * like this that isn't a problem and simplifies things.
+   */
+  for (var i = 0; i < 5; i++) {
+  
+          /* On for 1 second */
+          rpio.write(pin, rpio.HIGH);
+          rpio.sleep(1);
+  
+          /* Off for half a second (500ms) */
+          rpio.write(pin, rpio.LOW);
+          rpio.msleep(500);
+  }
 });
 
 // start the api
@@ -251,4 +54,3 @@ app.listen(8000, () => {
   console.log('api listening on port 8000');
   console.log();
 });
-
